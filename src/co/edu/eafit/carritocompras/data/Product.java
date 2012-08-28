7e4d4ef52181d5abada0f6f1b9a144bd72dc591d
Juan Restrepo <juandiegor@gmail.com>
2012-08-27 20:02:34 -0500

    Changed way of products initial data loading so tests are easier to build

--- src/co/edu/eafit/carritocompras/data/Product.java
+++ src/co/edu/eafit/carritocompras/data/Product.java
@@ -1,6 +1,8 @@
 package co.edu.eafit.carritocompras.data;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
 import co.edu.eafit.carritocompras.data.products.ElectronicProduct;
 import co.edu.eafit.carritocompras.data.products.Furniture;
@@ -11,17 +13,29 @@ public abstract class Product {
 	private String name;
 	private BigDecimal price;
 
+	private static final Map<String, BigDecimal> productPricesBD;
+	
+	static {
+		productPricesBD = new HashMap<String, BigDecimal>();
+		for (int i = 1; i < 6; i++) {
+			productPricesBD.put("EL-00"+i, new BigDecimal(Math.random()*100000));
+			productPricesBD.put("FU-00"+(i+5), new BigDecimal(Math.random()*1000000));
+		}
+		
+	}
 	
 	public abstract BigDecimal calculateDiscount();
 	
 	public static Product buildProduct(String code) {
-		if (code.startsWith("EL")) {
-			return new ElectronicProduct(code, "Electronic_"+code, new BigDecimal(Math.random()*100000));
-		} else if (code.startsWith("FU")) {
-			return new Furniture(code, "Furniture_"+code, new BigDecimal(Math.random()*1000000));
+		if (productPricesBD.get(code) == null) {
+			throw new IllegalArgumentException("Product does not exist. Products are EL-001 through EL-005 and FU-006 through FU-0010");
 		}
 		
-		throw new IllegalArgumentException("Code does not start with EL or FU");
+		if (code.startsWith("EL")) {
+			return new ElectronicProduct(code, "Electronic_"+code, productPricesBD.get(code));
+		} else if (code.startsWith("FU")) {
+			return new Furniture(code, "Furniture_"+code, productPricesBD.get(code));
+		}
 	}
 	
 	public String getCode() {
